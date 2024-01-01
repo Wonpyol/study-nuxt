fff9d8323411e39f570a0fab6cf2338d8b59dd4a
wonpyol <wonpyol@gmail.com>
2024-01-01 21:34:51 +0900

    feat: layout 사용법

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ $route.name }}</h1>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+      <li>상품</li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product"
+}
+</script>
+
+<style scoped>
+
+</style>
